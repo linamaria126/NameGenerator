80de725089b874525ff96d63aff422b27f979129
Lina Cardona <linamaria126@gmail.com>
2023-11-17 10:40:14 -0500

    first commit domain

--- Exercise-17.js
+++
@@ -1,9 +0,0 @@
-let myArray = [2323, 4344, 2325, 324413, 21234, 24531, 2123, 42234, 544, 456, 345, 42, 5445, 23, 5656, 423];
-let suma = 0;
-// Your code here
-
-for(let num of myArray){
-    suma += num;
-}
-
-console.log(suma);
